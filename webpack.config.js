80b4c08111bb2ea185ca905659d83c3de8afa2b5
Tangxinming <tangxinming@csii.com.cn>
2020-11-22 11:01:53 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin  } = require('webpack').container;
 
 module.exports = {
 
@@ -6,9 +7,12 @@ module.exports = {
     index: "./src/index.js",
   },
 
+  output: {
+    publicPath: 'http://localhost:12500'
+  },
+
   devServer: {
-    port: 8081,
-    disableHostCheck: true,
+    port: 12500,
   },
 
   module: {
@@ -28,6 +32,16 @@ module.exports = {
   },
 
   plugins: [
+    new ModuleFederationPlugin({
+      // 唯一ID，用于标记当前服务
+      name: 'app1',
+      // 提供给其他服务加载的文件
+      filename: 'remoteEntry.js',
+      // 需要暴露的模块，使用时通过 `${name}/${expose}` 引入
+      exposes: {
+        ZYHeader: './src/components/zy-header/ZYHeader',
+      },
+    }),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       chunks: ["index"],
